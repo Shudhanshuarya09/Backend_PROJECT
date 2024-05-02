8d7be09a81d38070c0b1b098d94e09b50308b20f
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-05-02 21:38:10 +0530

    info

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const users = require("./MOCK_DATA.json");
+
+const app = express();
+const PORT = 8000;
+
+//ROUTES
+
+
+
+
+
+app.listen(PORT, () => console.log(`server started at port 8000`));
