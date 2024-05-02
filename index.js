2d6f3b5327e4642809172cbfe8fbb6c06a9d0663
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-05-02 21:38:28 +0530

    info

--- index.js
+++ index.js
@@ -5,8 +5,60 @@ const app = express();
 const PORT = 8000;
 
 //ROUTES
+app.get("/users", (req, res) => {
+  const html = `
+  <ul>
+  ${users.map((user) => `<li>${user.first_name}</li>`)}
+  </ul>`;
+  res.send(html);
+});
 
 
+//REST API
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+  });
+
+  
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  return res.json(user);
+});
+
+app.post('/api/users', (req, res) => {
+  //ToDo: Create new user
+  return res.json({ status: "pending"});
+ });
+
+ app.patch('/api/users/:id', (req, res) => {
+  //ToDo: Update new user
+  return res.json({ status: "pending"});
+ });
+
+ app.delete('/api/users/:id', (req, res) => {
+  //ToDo: Delete the user
+  return res.json({ status: "pending"});
+ });
+
+
+ //also we can write like this because in route same information is passing('/api/users/:id') so in the future lets suppose you have to change something in the route then you are not going to edit changes in code individually in all the get, patch, delete http methods.
+
+app.route("/api/users.:id")
+
+.get((req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  return res.json(user);
+})
+.patch((req, res) => {
+  //ToDo: Update new user
+  return res.json({ status: "pending"});
+ })
+.delete((req, res) => {
+  //ToDo: Delete the user
+  return res.json({ status: "pending"});
+ });
 
 
 
